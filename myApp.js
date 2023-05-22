69da64ee9205cf1be65cd76665a648cd00f0896c
mdfakih <mf9049@gmail.com>
2023-05-22 18:34:44 +0530

    creating person model

--- myApp.js
+++ myApp.js
@@ -3,7 +3,21 @@ let mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+let Person = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  favoriteFoods: {
+    type: [String],
+    required: true
+  }
+});
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
