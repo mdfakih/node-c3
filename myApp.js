b94f3832d4512ea0181f73bc353d18d7ec21b119
mdfakih <mf9049@gmail.com>
2023-05-22 18:41:11 +0530

    corrected people schema

--- myApp.js
+++ myApp.js
@@ -3,14 +3,16 @@ let mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person = new mongoose.Schema({
+const personSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true
   },
   age: Number,
   favoriteFoods: [String]
-});
+})
+
+let Person = personSchema;
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
